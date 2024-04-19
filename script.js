a80707f6b60d0f314dd9e5799d1dbe200aa0d6bc
Ardit <arditselimi.dev@gmail.com>
2024-04-19 18:35:35 +0200

    meet the team section done

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ faqs.forEach((faq) => {
     }
   });
 });
+
+const workers = document.querySelectorAll(".team__item");
+
+workers.forEach((e) => {
+  const videoSrc = e.dataset.videoSrc;
+
+  const video = document.createElement("video");
+  video.src = videoSrc;
+  video.loop = true;
+  video.muted = true;
+  video.style.width = "100%";
+  video.style.height = "100%";
+  video.style.objectFit = "cover";
+  video.style.position = "absolute";
+  video.style.top = "0";
+  video.style.left = "0";
+  video.style.opacity = "0";
+
+  e.appendChild(video);
+
+  e.addEventListener("mouseover", () => {
+    video.play();
+    e.classList.add("active");
+    video.style.opacity = "1";
+    video.style.borderRadius = "10px";
+  });
+
+  e.addEventListener("mouseleave", () => {
+    e.classList.remove("active");
+    video.style.opacity = "0";
+  });
+});
